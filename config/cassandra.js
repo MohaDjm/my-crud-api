9ae3cd6687a1c086b8edbdaedc41f4fee13266fb
Mohamed <mohamedjoumoi@gmail.com>
2024-11-14 20:03:20 +0100

    feat(database): configure Cassandra client connection

--- config/cassandra.js
+++ config/cassandra.js
@@ -1,15 +1,18 @@
 const cassandra = require('cassandra-driver');
 require('dotenv').config();
 
+// Create a Cassandra client with connection details
 const client = new cassandra.Client({
   contactPoints: ['localhost'],
   localDataCenter: 'datacenter1',
   keyspace: 'test_keyspace',
 });
 
+// Connect to the Cassandra cluster
 client
   .connect()
   .then(() => console.log('Connected to Cassandra'))
   .catch((err) => console.error('Failed to connect to Cassandra', err));
 
+// Export the client for use in other modules
 module.exports = client;
